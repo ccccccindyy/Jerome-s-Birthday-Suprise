31f908f1afaba6b9d35c97f8f3af54595ddc2642
Xin Zhang <xzhang7@deloitte.ie>
2020-09-09 01:44:35 +0100

    Updates

---
+++ precache-manifest.90698489a95fc8c961d6313d5cbfd817.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1baf94a1dab4afe4d1554318196b90",
+    "url": "/Jerome-s-Birthday-Surprise/index.html"
+  },
+  {
+    "revision": "d5f6708ef9000c027517",
+    "url": "/Jerome-s-Birthday-Surprise/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "4464337ae23b3812cf9c",
+    "url": "/Jerome-s-Birthday-Surprise/static/css/main.2603fece.chunk.css"
+  },
+  {
+    "revision": "d5f6708ef9000c027517",
+    "url": "/Jerome-s-Birthday-Surprise/static/js/2.b5d2f2be.chunk.js"
+  },
+  {
+    "revision": "e7c69aa4549a0938e155d41214f1973f",
+    "url": "/Jerome-s-Birthday-Surprise/static/js/2.b5d2f2be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4464337ae23b3812cf9c",
+    "url": "/Jerome-s-Birthday-Surprise/static/js/main.fa7bbb3d.chunk.js"
+  },
+  {
+    "revision": "cad47fbe6f62f124169d",
+    "url": "/Jerome-s-Birthday-Surprise/static/js/runtime-main.7401b75f.js"
+  },
+  {
+    "revision": "499b1af23b70fd91390f3de88e9fa753",
+    "url": "/Jerome-s-Birthday-Surprise/static/media/cranky.499b1af2.png"
+  },
+  {
+    "revision": "a8775209a9b4cc60879d628c9e791ee1",
+    "url": "/Jerome-s-Birthday-Surprise/static/media/jer0.a8775209.png"
+  },
+  {
+    "revision": "5b54aebbaedd63924cf0345584f5f771",
+    "url": "/Jerome-s-Birthday-Surprise/static/media/jer1.5b54aebb.png"
+  },
+  {
+    "revision": "7a893ec121ea5b9969cc85c2dd8e0627",
+    "url": "/Jerome-s-Birthday-Surprise/static/media/jer2.7a893ec1.png"
+  }
+]);
